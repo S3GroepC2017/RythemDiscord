78f06d97ce2a717eceec4fcbe7603a0ce9e18c08
larsvandriel <lars.van.driel@hotmail.com>
2017-09-25 11:48:19 +0200

    Klassen, interfaces en enums aangemaakt. ILogic Functies geimplementeerd en alle enums geimplementeerd

---
+++ src/logic/GameManager.java
@@ -0,0 +1,51 @@
+package logic;
+
+/**
+ * Created by Lars on 25-9-2017.
+ */
+public class GameManager  implements ILogic{
+    @Override
+    public int createGame(String playerNameHost, IGameMode gameMode, GameDifficulty gameDifficulty) {
+        return 0;
+    }
+
+    @Override
+    public boolean joinLobby(String playerName, int lobbyNumber) {
+        return false;
+    }
+
+    @Override
+    public boolean leaveLobby(Player player) {
+        return false;
+    }
+
+    @Override
+    public Stave startGame() {
+        return null;
+    }
+
+    @Override
+    public char[] showNotes(Player player) {
+        return new char[0];
+    }
+
+    @Override
+    public boolean playNote(char pressedKey, Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean pauseGame() {
+        return false;
+    }
+
+    @Override
+    public boolean resumeGame() {
+        return false;
+    }
+
+    @Override
+    public boolean quitGame() {
+        return false;
+    }
+}
---
+++ src/logic/GameStatus.java
@@ -0,0 +1,11 @@
+package logic;
+
+/**
+ * Created by Lars on 25-9-2017.
+ */
+public enum GameStatus {
+    Quit,
+    Running,
+    Paused,
+    Finished
+}
