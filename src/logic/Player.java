83b6a6604aabfea5efb1f9c101a46a232b69893f
larsvandriel <lars.van.driel@hotmail.com>
2017-10-02 11:01:31 +0200

    Begin gemaakt met het implementeren van het klassendiagram. Nog te doen zijn de lobby, de game klasse, de IGameMode interface en de Gamemodes

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -1,7 +1,34 @@
 package logic;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by Lars on 25-9-2017.
  */
 public class Player implements IPlayable {
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean addPoints(int amountOfPoints) {
+        if (amountOfPoints <= 0) {
+            return false;
+        }
+        score += amountOfPoints;
+        return true;
+    }
 }
--- src/logic/Stave.java
+++ src/logic/Stave.java
@@ -4,4 +4,28 @@ package logic;
  * Created by Lars on 25-9-2017.
  */
 public class Stave {
+    private char[] node;
+    private NodeType type;
+    private Player player;
+
+    public Stave(char[] node, NodeType type, Player player) {
+        this.node = node;
+        this.type = type;
+        this.player = player;
+    }
+
+    public char[] getNode()
+    {
+        return node;
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+    public NodeType getType()
+    {
+        return type;
+    }
 }
