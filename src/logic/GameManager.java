e6a40e19e9ba05d43dc1146d24b2f6d90023e0a8
hazanko <joe.vankollenburg@student.fontys.nl>
2017-10-23 14:13:42 +0200

    implemented new klassendiagram

--- src/logic/GameManager.java
+++ src/logic/GameManager.java
@@ -6,60 +6,25 @@ import java.util.List;
  * Created by Lars on 25-9-2017.
  */
 public class GameManager  implements ILogic{
-    @Override
-    public int createGame(String playerNameHost, IGameMode gameMode, GameDifficulty gameDifficulty) {
-        return 0;
+
+    private Game currentGame;
+
+    public GameManager(){
+        currentGame = new Game();
     }
 
     @Override
-    public boolean joinLobby(String playerName, int lobbyNumber) {
-        return false;
+    public void startGame() {
+
     }
 
     @Override
-    public boolean leaveLobby(Player player) {
-        return false;
+    public char[] getNodes() {
+        return currentGame.getNodes();
     }
 
     @Override
-    public List<Team> getTeams(Lobby lobby)
-    {
-        return null;
-    }
-
-    @Override
-    public boolean joinTeam(Player player, Team team)
-    {
-        return false;
-    }
-
-    @Override
-    public Stave startGame() {
-        return null;
-    }
-
-    @Override
-    public char[] showNotes(Player player) {
-        return new char[0];
-    }
-
-    @Override
-    public boolean playNote(char pressedKey, Player player) {
-        return false;
-    }
-
-    @Override
-    public boolean pauseGame() {
-        return false;
-    }
-
-    @Override
-    public boolean resumeGame() {
-        return false;
-    }
-
-    @Override
-    public boolean quitGame() {
+    public boolean keyPressed(char keyPressed) {
         return false;
     }
 }
