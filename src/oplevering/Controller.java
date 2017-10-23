2eb7c462156c5d4343fb44b1f571845f882566ce
Niels Westland <niels@MacBook-Pro-van-Niels.local>
2017-10-23 14:26:58 +0200

    Hersteld naar Reboot, GUI files toegevoegd

---
+++ src/oplevering/Controller.java
@@ -0,0 +1,37 @@
+package oplevering;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import logic.GameManager;
+import logic.ILogic;
+
+public class Controller
+{
+    private ILogic logic;
+
+    public Controller()
+    {
+        this.logic = new GameManager();
+    }
+
+    @FXML private Label mainKey;
+    @FXML private Label futKey1;
+    @FXML private Label futKey2;
+    @FXML private Label futKey3;
+
+    @FXML private Pane mainPane;
+
+    //TODO
+    @FXML protected void keyPressed(KeyEvent event)
+    {
+        mainKey.setText(event.getCode().toString());
+    }
+
+    @FXML protected void initialize()
+    {
+        Platform.runLater(() -> mainPane.requestFocus());
+    }
+}
