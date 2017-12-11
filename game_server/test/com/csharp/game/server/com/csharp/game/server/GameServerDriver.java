62420ccd3a50225b77a4c04d614af4e121f69c2f
larsvandriel <lars.van.driel@hotmail.com>
2017-12-11 21:04:18 +0100

    Driver Gameserver toegevoegd voor testing.

--- game_server/test/com/csharp/game/server/com/csharp/game/server/GameServerDriver.java
+++ game_server/test/com/csharp/game/server/com/csharp/game/server/GameServerDriver.java
@@ -1,8 +1,36 @@
 package com.csharp.game.server.com.csharp.game.server;
 
+import com.csharp.sharedclasses.KeyPressedResult;
+import com.csharp.sharedclasses.Player;
 import com.csharp.sharedclasses.fontyspublisher.IPropertyListener;
+import com.csharp.sharedclasses.fontyspublisher.IRemotePropertyListener;
 
-public class GameServerDriver implements IPropertyListener
+import java.awt.image.Kernel;
+import java.beans.PropertyChangeEvent;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Objects;
+
+public class GameServerDriver implements IRemotePropertyListener
 {
+    public List<Player> players;
+    public int nodeListIndex;
+    public KeyPressedResult keyPressedResult;
 
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) throws RemoteException
+    {
+        if(evt.getPropertyName().equals("Player"))
+        {
+            players = (List<Player>)evt.getNewValue();
+        }
+        else if(evt.getPropertyName().equals("noteListIndex"))
+        {
+            nodeListIndex = (int) evt.getNewValue();
+        }
+        else if(evt.getPropertyName().equals("lastKeyPressResult"))
+        {
+            keyPressedResult = (KeyPressedResult) evt.getNewValue();
+        }
+    }
 }
